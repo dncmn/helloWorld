e2f0764ec7007fbd6a7ff09b664abdd81539f3fe
manan <manan@snaplingo.com>
2019-04-24 12:56:05 +0800

    new Message map

--- client.go
+++ client.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"google.golang.org/grpc"
 	pb "helloWorld/pb"
-	"log"
 )
 
 const (
@@ -14,60 +11,84 @@ const (
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	//c := pb.NewGreeterClient(conn)
-	//name := defaultName
-	//if len(os.Args) > 1 {
-	//	name = os.Args[1]
-	//}
-	//
-	//r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	//conn, err := grpc.Dial(address, grpc.WithInsecure())
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
-	//fmt.Println("greeting:", r.Message)
-	//loginReply, err := c.Login(context.Background(), &pb.LoginRequest{Name: "root", Password: "123456"})
+	//defer conn.Close()
+	////c := pb.NewGreeterClient(conn)
+	////name := defaultName
+	////if len(os.Args) > 1 {
+	////	name = os.Args[1]
+	////}
+	////
+	////r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	////if err != nil {
+	////	log.Fatal(err)
+	////}
+	////fmt.Println("greeting:", r.Message)
+	////loginReply, err := c.Login(context.Background(), &pb.LoginRequest{Name: "root", Password: "123456"})
+	////if err != nil {
+	////	log.Fatal(err)
+	////}
+	//////fmt.Println(loginReply.Message)
+	//////fmt.Println(loginReply.Color)
+	//////fmt.Println(loginReply.RewardMap)
+	////fmt.Println(loginReply.DateList)
+	////
+	////canSetResp, err := c.CanSet(context.Background(), &pb.CanSetRequest{})
+	////if err != nil {
+	////	log.Fatal(err)
+	////}
+	////fmt.Println(canSetResp.CanSet)
+	////
+	////re, err := c.CanUpdate(context.Background(), &pb.CanUpdateRequest{Username: "root"})
+	////if err != nil {
+	////	fmt.Println(err)
+	////}
+	////fmt.Println("final=", re)
+	////fmt.Println(pb.UserRegisterType_GuestRegister, pb.UserRegisterType_NormalRegister)
+	//
+	//u := pb.NewUserClient(conn)
+	//r, err := u.Register(context.Background(), &pb.RegisterRequest{Username: "manan", Password: "123456", Country: 1, PhoneNum: "15737345574"})
 	//if err != nil {
 	//	log.Fatal(err)
+	//	return
 	//}
-	////fmt.Println(loginReply.Message)
-	////fmt.Println(loginReply.Color)
-	////fmt.Println(loginReply.RewardMap)
-	//fmt.Println(loginReply.DateList)
-	//
-	//canSetResp, err := c.CanSet(context.Background(), &pb.CanSetRequest{})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(canSetResp.CanSet)
-	//
-	//re, err := c.CanUpdate(context.Background(), &pb.CanUpdateRequest{Username: "root"})
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("final=", re)
-	//fmt.Println(pb.UserRegisterType_GuestRegister, pb.UserRegisterType_NormalRegister)
+	//fmt.Println(r.Uid)
+	////switch  pb.Profile{}.Avatar.(type){
+	////case *pb.Profile_ImageData:
+	////		fmt.Println("aaaaa")
+	////case *pb.Profile_ImageUrl:
+	////		fmt.Println("bbbbb")
+	////default:
+	////		fmt.Println("cccc")
+	////
+	////}
+	t := pb.SearchRequest{}
+	t.List = make(map[int32]*pb.ListDate)
+	l1 := &pb.ListDate{}
+	l1.List = make(map[int32]int32)
+	l1.List[1] = 1
+	l1.List[2] = 1
+	l1.List[3] = 1
+	l1.List[4] = 1
 
-	u := pb.NewUserClient(conn)
-	r, err := u.Register(context.Background(), &pb.RegisterRequest{Username: "manan", Password: "123456", Country: 1, PhoneNum: "15737345574"})
-	if err != nil {
-		log.Fatal(err)
-		return
+	l2 := &pb.ListDate{}
+	l2.List = make(map[int32]int32)
+	l2.List[1] = 10
+	l2.List[2] = 10
+	l2.List[3] = 10
+	l2.List[4] = 10
+	t.List = map[int32]*pb.ListDate{
+		1: l1,
+		2: l2,
 	}
-	fmt.Println(r.Uid)
-	switch  pb.Profile{}.Avatar.(type){
-	case *pb.Profile_ImageData:
-			fmt.Println("aaaaa")
-	case *pb.Profile_ImageUrl:
-			fmt.Println("bbbbb")
-	default:
-			fmt.Println("cccc")
-
+	fmt.Println(t.List)
+	for key, val := range t.List {
+		fmt.Printf("key=%v,val=%v\n", key, val)
+		for boxID, res := range val.List {
+			fmt.Printf("----------boxID=%v,result=%v\n", boxID, res)
+		}
 	}
-
-
 }
--- server.go
+++ server.go
@@ -26,8 +26,7 @@ func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply
 	color := make([]string, 0)
 	rewardMap := make(map[int32]bool)
 	DateList := make(map[int32]*pb.List)
-
-	if in.Name == "root" && in.Password == "123456" {
+	if in.Username == "root" && in.Password == "123456" {
 		msg = "login success"
 		color = []string{"black", "red", "yellow"}
 		rewardMap[1001] = true
@@ -38,8 +37,8 @@ func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply
 			1: &pb.List{Id: []int32{1, 2, 3, 4}},
 		}
 	}
-
-	return &pb.LoginReply{Message: msg, Color: color, RewardMap: rewardMap, DateList: DateList}, nil
+	fmt.Println(msg, color, DateList)
+	return &pb.LoginReply{}, nil
 }
 
 func (s *server) CanSet(ctx context.Context, in *pb.CanSetRequest) (*pb.CanSetReply, error) {
@@ -51,6 +50,14 @@ type user struct{}
 
 func (u *user) Register(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterReply, error) {
 	return &pb.RegisterReply{Uid: "123456"}, nil
+}
+
+func (u *user) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply, error) {
+	return &pb.LoginReply{}, nil
+}
+
+func (u *user) UserByUID(ctx context.Context, in *pb.UserByUIDRequest) (*pb.UserByUIDReply, error) {
+	return &pb.UserByUIDReply{}, nil
 }
 
 func (s *server) CanUpdate(ctx context.Context, in *pb.CanUpdateRequest) (resp *pb.CanUpdateReply, err error) {
